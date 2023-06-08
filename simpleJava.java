1a7f535ab2c6dcce1781baabc1e10115073cf059
Kumar S <kumar.sambhuni@semanticbits.com>
2023-06-08 00:23:18 -0400

    Update simpleJava.java

--- simpleJava.java
+++ simpleJava.java
@@ -4,6 +4,6 @@
  */
 public class simpleJava {
   public static void main(String[] args) {
-    System.out.println("This is a simple Java Code!!!");
+    System.out.println("This is a simple Java Code");
   }
 }
