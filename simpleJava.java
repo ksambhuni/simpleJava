67ba031ce159316dd0cea5c8a3320e8a12473977
Kumar S <kumar.sambhuni@semanticbits.com>
2023-06-08 00:06:00 -0400

    Create simpleJava.java

---
+++ simpleJava.java
@@ -0,0 +1,9 @@
+
+/** 
+ * The simpleJava class implements an application that simply prints to standard output.
+ */
+public class simpleJava {
+  public static void main(String[] args) {
+    System.out.println("This is a simple Java Code!!!");
+  }
+}
